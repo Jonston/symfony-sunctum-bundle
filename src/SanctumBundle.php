b34879a4110af19d70a350c8359ef17f8441fdaf
Eugene <jonston85@gmail.com>
2025-09-05 14:11:15 +0300

    Refactoring

--- src/SanctumBundle.php
+++ src/SanctumBundle.php
@@ -2,8 +2,14 @@
 
 namespace Jonston\SanctumBundle;
 
+use Jonston\SanctumBundle\DependencyInjection\SanctumExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SanctumBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new SanctumExtension();
+    }
 }
