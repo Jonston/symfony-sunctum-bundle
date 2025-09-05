07c9376b1ed7f157a13d19330e56abef566409fa
Eugene <jonston85@gmail.com>
2025-09-05 13:29:23 +0300

    Refactoring

--- src/DependencyInjection/SanctumExtension.php
+++ src/DependencyInjection/SanctumExtension.php
@@ -2,6 +2,7 @@
 
 namespace Jonston\SanctumBundle\DependencyInjection;
 
+use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -9,6 +10,9 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class SanctumExtension extends Extension
 {
+    /**
+     * @throws Exception
+     */
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
